c88a3c13e018342e196ce1c96376fef5a441b8a9
robertadau <98342454+robertadau@users.noreply.github.com>
2022-01-25 19:51:26 +0200

    sutvarkeme failu struktura

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'kaunas-coding-school/web',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kaunas-coding-school/web' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
