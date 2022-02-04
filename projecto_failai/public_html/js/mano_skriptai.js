9245814c639813ac61451da7d48ac778e4c48295
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-04 19:51:37 +0200

    pakeiciau

--- projecto_failai/public_html/js/mano_skriptai.js
+++ projecto_failai/public_html/js/mano_skriptai.js
@@ -1,28 +1,63 @@
-let x = 5;
-let y = 6;
+let a = -5;
+let b = 0;
+let c = -4;
+let d = 4;
+let x = -4;
+let y = 0;
 
-if (x == 5) {
-    let msg = 'Pirma Salyga atitinka';
-    window.alert('Salyga atitinka');
+if (a === -5 || d === 4) {
+    let msg = 'Intervalu sankirta egzistuoja';
+    window.alert('Pirmas sankirtos rezis');
     console.log(msg);
 }
 
-if (x === 5 || y === 6) {
-    let msg = 'Antra Salyga atitinka';
+if (a === -5 || b === 0) {
+    let msg = 'Antras sankirtos rezis';
     window.alert(msg);
     console.log(msg);
 }
 
-if (x !== 4 && y !== 6) {
-    let msg = 'Trecia Salyga atitinka';
+if (a === -5 || c === -4) {
+    let msg = 'Neegzistuoja sankirtos rezis';
     window.alert(msg);
     console.log(msg);
-} else if (x === '5' || y === 'Jonas') {
-    let msg = 'Bplanas treciai salygai';
+
+}
+
+if (c === -4 || b === 0 ) {
+    let msg = 'Trecias sankirtos rezis';
+    window.alert(msg);
+    console.log(msg);
+}
+
+if (c === -4 || d === 4) {
+    let msg = 'Ketvirtas sankirtos rezis';
+    window.alert(msg);
+    console.log(msg);
+}
+
+if (b === 0 || d === 4) {
+    let msg = 'Neegzistuoja sankirtos rezis';
+    window.alert(msg);
+    console.log(msg);
+    }
+if (x === -4 || y === 0) {
+    let msg = 'Penktas sankirtos rezis';
+    window.alert(msg);
+    console.log(msg);
+}
+
+if (x === -4 || y === 0) {
+    let msg = 'Penktas sankirtos rezis';
+    window.alert(msg);
+    console.log(msg);
+}
+else if (x === '-4' || y === '0') {
+    let msg = 'rezis';
     window.alert(msg);
     console.log(msg);
 } else {
-    let msg = 'Trecios Salygos atsarginis planas';
+    let msg = 'egzistuoja rezis';
     window.alert(msg);
     console.log(msg);
 }
