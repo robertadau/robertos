3051de98eb8d3866b72d4311b26e8762c216ca22
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-04 19:05:06 +0200

    pakeiciau

--- projecto_failai/public_html/js/mano_skriptai.js
+++ projecto_failai/public_html/js/mano_skriptai.js
@@ -1,3 +1,32 @@
+let x = 5;
+let y = 6;
+
+if (x == 5) {
+    let msg = 'Pirma Salyga atitinka';
+    window.alert('Salyga atitinka');
+    console.log(msg);
+}
+
+if (x === 5 || y === 6) {
+    let msg = 'Antra Salyga atitinka';
+    window.alert(msg);
+    console.log(msg);
+}
+
+if (x !== 4 && y !== 6) {
+    let msg = 'Trecia Salyga atitinka';
+    window.alert(msg);
+    console.log(msg);
+} else if (x === '5' || y === 'Jonas') {
+    let msg = 'Bplanas treciai salygai';
+    window.alert(msg);
+    console.log(msg);
+} else {
+    let msg = 'Trecios Salygos atsarginis planas';
+    window.alert(msg);
+    console.log(msg);
+}
+
 
 
 
