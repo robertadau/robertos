b05bf5866f6b83dc874f652a233a4c732726664a
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-07 15:03:05 +0200

    pakeiciau

--- projecto_failai/public_html/js/mano_skriptai.js
+++ projecto_failai/public_html/js/mano_skriptai.js
@@ -9,10 +9,27 @@ if (a > b || d < c || d < a || c > b) {
     window.alert('Sankirta negalima');
 } else {
     if (c <= a && a <= d) {
-        x = 'a=' + a;
+        x = a;
     }
+    if (a <= c && c <= d) {
+        x = c;
+    }
+    if (a <= d && d <= b) {
+        y = d;
+    }
+    if (c <= b && b <= d) {
+        y = b;
+    }
+
+// naujas kintamasis 'atsakymas' yra lygus x ir y stringu sudetis
+    let atsakymas = x + ' ir ' + y
+
+//spausdinam 'Rasta sankirta: ' + x + 'ir' + y
+    document.getElementById('ats').innerHTML = 'Rasta sankirta: ' + atsakymas;
+
+    document.getElementById('ats').innerHTML = 'Rasta sankirta: ' + x + ' ir ' + y;
 }
 
-if (a)
+
 
 
