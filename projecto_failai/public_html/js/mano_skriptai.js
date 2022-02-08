65b28d79e83ce0bd5c84040ee2a77992df37743a
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-08 19:30:28 +0200

    h

--- projecto_failai/public_html/js/mano_skriptai.js
+++ projecto_failai/public_html/js/mano_skriptai.js
@@ -1,73 +1,13 @@
+let lemposBusena = false;
+let lempa = document.getElementById('lempute');
 
-for (let i = 0; i < 3; ++i) {
-    masyvas.push(i+'_elem');
+function toggleLamp () {
+    if (lemposBusena)   {
+        lempa.src =
+            'https://previews.123rf.com/images/ericmilos/ericmilos0912/ericmilos091200136/6109526-3d-render-of-light-bulb-on-white.jpg';
+    } else {
+        lempa.src =
+            'https://previews.123rf.com/images/murika/murika1511/murika151100069/48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.jpg';
+    }
+    lemposBusena = !lemposBusena;
 }
-
-
-
-for (let h = 0; h < masyvas.length; ++h) {
-    console.log('ELEM: ' + masyvas[h]);
-}
-
-
-
-let masyvas = ['a', 'b', 5, 8, 'Jonas', true];
-
-for (let i = 0; i < 6; ++i) {
-    let elementas = masyvas[i];
-    console.log('FOR ciklas: ' + elementas);
-    suma += elementas + '-';
-}
-
-
-
-let j = 10;
-while(j >= 1) {
-    console.log('WHILE ciklas ' + j);
-    j--;
-}
-
-
-
-let j = masyvas.length - 1;
-while(j >= 0) {
-    console.log('WHILE ciklas ' + j + '=' + elementas);
-    j--;
-}
-
-
-let z = 0;
-do {
-   let elementas = masyvas[z];
-    console.log('WHILE ciklas ' + z + '=' + elementas);
-    z++;
-} while (z <= masyvas.length - 1);
-
-
-
-let masyvas = ['a', 'b', 5, 8, 'Jonas', true];
-
-masyvas[0] = 'Tautvydas';
-masyvas[3] = 'Dulskis';
-
-arr.push('kestas');
-arr[0] = 'Petras';
-
-for (let i = 0; i < 3; ++i) {
-    masyvas.unshift(items: i+'_elem');
-}
-
-for (let i = 0; i < 3; ++i) {
-    masyvas.push(i+'_elem');
-}
-
-
-let asmuo = {};
-
-asmuo.akiuSpalva = 'melyna';
-asmuo.amzius = 35;
-asmuo.ugis = 180;
-
-let akius = asmuo.akiuSpalva;
-
-console.log(asmuo);
