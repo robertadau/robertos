6e17b3927bdedf3519c12e9929e2f478ab9a8ca0
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-19 14:02:39 +0200

    pakeiciau

--- projecto_failai/public_html/temperatures.php
+++ projecto_failai/public_html/temperatures.php
@@ -7,19 +7,23 @@ $temp_array_length = count($temp_array);
 foreach($temp_array as $temp) {
     $tot_temp += $temp;
 }
-
 $avg_high_temp = $tot_temp/$temp_array_length;
 echo "Vidutinė temperatūra : ".$avg_high_temp."
 ";
 sort($temp_array);
+echo "<br>";
 echo " Penkios žemiausios temperatūros : " ;
 for ($i=0; $i< 5; $i++)
 {
     echo $temp_array[$i].", ";
 }
+echo "<br>";
 echo "Penkios aukščiausios temperatūros :";
 for ($i=($temp_array_length-5); $i< ($temp_array_length); $i++)
 {
     echo $temp_array[$i].", ";
 }
 ?>
+
+
+
