7287fa91e7edd21570fbee7ceb0838e7c13e71e3
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-22 20:54:51 +0200

    Pakeiciau

--- projecto_failai/public_html/prisijungti.php
+++ projecto_failai/public_html/prisijungti.php
@@ -3,12 +3,12 @@
 session_start();
 
 
-if (!empty($_POST)) &&
+if (!empty($_POST) &&
     isset($_POST['username']) &&
     isset($_POST['password']) &&
     $_POST['username'] === 'admin' &&
     $_POST['password'] === 'admin'
-) {
+ ) {
     $_SESSION['username'] = $_POST['username'];
 
     header('location: admin.php');
