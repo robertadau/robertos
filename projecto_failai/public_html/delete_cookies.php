034204a3edb10f253d4decc51d45e839be1ca855
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-28 21:00:06 +0200

    p

--- projecto_failai/public_html/delete_cookies.php
+++ projecto_failai/public_html/delete_cookies.php
@@ -1,5 +1,10 @@
 <?php
 
+if(array_key_exists('delete', $_GET)) {
+    setcookie('vartotojas', null, 0, '/');
+    unset($_COOKIE['vartotojas']);
+}
+
 if(!isset($_COOKIE['vartotojas'])) {
     setcookie('vartotojas', 'Roberta Daugėlaitė', time() + 60, '/');
     echo 'Sausainėlis pavadinimu "vartotojas" nera sukurtas! Mes jį ką tik sukūrėme';
@@ -9,7 +14,3 @@ if(!isset($_COOKIE['vartotojas'])) {
 
 }
 
-if(array_key_exists('delete', $_GET)) {
-    setcookie('vartotojas', null, 0, '/');
-    unset($_COOKIE['vartotojas']);
-}
