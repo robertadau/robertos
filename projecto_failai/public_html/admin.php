3eeb0751e8b4e4bb6b195fe99daca1f889e81582
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-28 21:17:37 +0200

    p

--- projecto_failai/public_html/admin.php
+++ projecto_failai/public_html/admin.php
@@ -6,16 +6,6 @@ if (array_key_exists('username', $_SESSION)) {
     echo 'Tai ADMIN panele';
     echo '<br><a href="logout.php">Atsijungti</a>';
 
-    include '../src/Entities/Car.php';
-
-    $car = new Car();
-    $car->color = 'red';
-    echo 'We are driving' . $car->getColor() . 'car<br>';
-    echo 'Current speed is: ' . $car->getCurrentSpeed() . '<br>';
-    echo 'Current milage is: ' . $car->getMilage() . '<br>';
-    $newSpeed = '50km/h';
-    echo 'Currently we are driving' . $car->getColor() . 'car at' . $newSpeed . '<br>';
-
 
 }  else {
     header('location: login.html');
