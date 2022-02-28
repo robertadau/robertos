bb563daa30007e16e9398c5aa442f357b232ee62
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-28 19:51:15 +0200

    p

--- projecto_failai/public_html/cookies.php
+++ projecto_failai/public_html/cookies.php
@@ -1,12 +1,9 @@
 <?php
 
-var_dump($_COOKIE);
-
 $sausainelio_vardas = 'vartotojas';
 $sausainelio_reiksme = 'Roberta Daugėlaitė';
 setcookie($sausainelio_vardas, $sausainelio_reiksme, time() + 10, '/');
 // 86400 = 1 diena
-
 
 if(!isset($_COOKIE['vartotojas'])) {
     echo 'Sausainėlis pavadinimu "vartotojas" nera sukurtas! Mes jį ką tik sukūrėme';
@@ -14,4 +11,5 @@ if(!isset($_COOKIE['vartotojas'])) {
     echo 'Sausainėlis "vartotojas" yra sukurtas!<br>';
     echo 'Jo reikšmė yra: ' . $_COOKIE['vartotojas'];
 }
+var_dump($_COOKIE);
 ?>
