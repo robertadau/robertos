dc81dc119c55292806bfaf94a1f7d9bfd08774e3
robertadau <98342454+robertadau@users.noreply.github.com>
2022-02-28 15:48:52 +0200

    pakeiciau

--- projecto_failai/public_html/kontaktai.php
+++ projecto_failai/public_html/kontaktai.php
@@ -1,6 +1,6 @@
 <?php
 $name = $_POST['name'];
-$visitor_email = $_POST['email'];
+$email = $_POST['email'];
 $message = $_POST['message'];
 
 $email_from = 'daugelaite.roberta@gmail.com';
@@ -8,14 +8,14 @@ $email_from = 'daugelaite.roberta@gmail.com';
 $email_subject = "New Form Submission";
 
 $email_body = "Jūsų vardas: $name.\n".
-                "Jūsų el. paštas: $visitor_email.\n".
+                "Jūsų el. paštas: $email.\n".
                 "Žinutė: $message.\n";
 
 $to = "vaidilute20@yahoo.com";
 
 $headers = "From: $email_from \r\n";
 
-$headers .= "Reply_To: $visitor_email \r\n";
+$headers .= "Reply_To: $email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
