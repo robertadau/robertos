361566e486d04eea801d1ce73870e70ccd2bccae
robertadau <98342454+robertadau@users.noreply.github.com>
2022-03-02 12:56:10 +0200

    p

--- projecto_failai/public_html/js/meniu.js
+++
@@ -1,8 +0,0 @@
-($)function() {
-    var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
-    $('nav > a').click(function(){
-        if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-            $(this).addClass("active");
-        // $(this).parent("li").addClass("active");
-    })
-});
